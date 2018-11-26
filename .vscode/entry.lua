ffb5e9f59a53795befbbe9339971a57e2bcedf34
sm0kin <sm0kin.mod@gmail.com>
2018-11-26 11:09:30 +0100

    commit

--- .vscode/entry.lua
+++ .vscode/entry.lua
@@ -1,6 +1,7 @@
 require("sm0/export_helpers_sm0_confed")
 require("sm0/export_helpers_sm0_morathi")
 require("sm0/export_helpers_sm0_mazdamundi")
+require("sm0/export_helpers_sm0_fay")
 require("sm0/export_helpers_sm0_abandon")
 require("sm0/export_helpers_sm0_save_cam")
 require("sm0/table_save")
