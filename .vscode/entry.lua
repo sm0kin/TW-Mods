a4c58129df93e95b3f6f87bad73822f8c6218ac4
sm0kin <sm0kin.mod@gmail.com>
2019-02-26 16:30:20 +0100

    New structure

--- .vscode/entry.lua
+++ .vscode/entry.lua
@@ -1,41 +1,42 @@
---require("sm0/export_helpers_sm0_confed")
-require("sm0/campaign/mod/sm0_confed")
+require("sm0/sm0_force_confederation/script/export_helpers_sm0_confed")
+--require("sm0/campaign/mod/sm0_confed")
 
---require("sm0/export_helpers_sm0_abandon")
-require("sm0/campaign/mod/sm0_abandon")
+require("sm0/sm0_abandon/script/export_helpers_sm0_abandon")
+--require("sm0/sm0_abandon/script/campaign/mod/sm0_abandon")
 
-require("sm0/export_helpers_sm0_save_cam")
-require("sm0/table_save")
-require("sm0/campaign/mod/sm0_test")
---require("sm0/temp")
-require("sm0/export_helpers_sm0_hidden_legendaries")
+require("sm0/sm0_save_cam_settings/script/export_helpers_sm0_save_cam")
+require("sm0/sm0_save_cam_settings/script/table_save")
 
-require("sm0/export_helpers_sm0_mixedLores")
-require("sm0/ml_tables/ml_wh2_main_lzd_lord_mazdamundi")
-require("sm0/ml_tables/ml_wh2_main_lzd_slann_mage_priest")
-require("sm0/ml_tables/ml_wh2_main_def_morathi")
-require("sm0/ml_tables/ml_wh2_main_hef_teclis")
-require("sm0/ml_tables/ml_wh2_dlc10_hef_alarielle")
-require("sm0/ml_tables/ml_chs_archaon")
-require("sm0/ml_tables/ml_dlc03_bst_malagor")
-require("sm0/ml_tables/ml_dlc07_brt_fay_enchantress")
---require("sm0/ml_coloured_text_button")
+require("sm0/sm0_test/script/campaign/mod/sm0_test")
+
+require("sm0/@hidden_legendaries/script/export_helpers_sm0_hidden_legendaries")
+
+require("sm0/sm0_mixedlores/script/export_helpers_sm0_mixedLores")
+require("sm0/sm0_mixedlores/script/ml_tables/ml_wh2_main_lzd_lord_mazdamundi")
+require("sm0/sm0_mixedlores/script/ml_tables/ml_wh2_main_lzd_slann_mage_priest")
+require("sm0/sm0_mixedlores/script/ml_tables/ml_wh2_main_def_morathi")
+require("sm0/sm0_mixedlores/script/ml_tables/ml_wh2_main_hef_teclis")
+require("sm0/sm0_mixedlores/script/ml_tables/ml_wh2_dlc10_hef_alarielle")
+require("sm0/sm0_mixedlores/script/ml_tables/ml_chs_archaon")
+require("sm0/sm0_mixedlores/script/ml_tables/ml_dlc03_bst_malagor")
+require("sm0/sm0_mixedlores/script/ml_tables/ml_dlc07_brt_fay_enchantress")
+--require("sm0/sm0_mixedlores/script/ml_coloured_text_button")
 --require("C:/Program Files (x86)/Steam/steamapps/common/Total War WARHAMMER II/data/script/export_helpers_sm0_mixedLores")
 
---require("sm0/campaign/mod/unlocker_uc")
-require("C:/Program Files (x86)/Steam/steamapps/common/Total War WARHAMMER II/data/script/campaign/mod/unlocker_uc")
---require("sm0/TEST/unlocker_uc/campaign/mod/ultimate_chaos")
-require("C:/Program Files (x86)/Steam/steamapps/common/Total War WARHAMMER II/data/script/campaign/mod/ultimate_chaos")
+require("sm0/!!unlocker_uc/script/campaign/mod/unlocker_uc")
+--require("C:/Program Files (x86)/Steam/steamapps/common/Total War WARHAMMER II/data/script/campaign/mod/unlocker_uc")
 
+require("sm0/_ultimate_chaos_wh2/script/campaign/mod/ultimate_chaos")
+--require("C:/Program Files (x86)/Steam/steamapps/common/Total War WARHAMMER II/data/script/campaign/mod/ultimate_chaos")
+--require("C:/Program Files (x86)/Steam/steamapps/common/Total War WARHAMMER II/data/script/campaign/main_warhammer/wh_dlc08_norscan_gods")
+--require("C:/Program Files (x86)/Steam/steamapps/common/Total War WARHAMMER II/data/script/campaign/main_warhammer/wh_chaos_invasion")
+
+--require("sm0/sm0_battle_speed/script/battle/mod/battle_speed")
 
 --require("sm0/ml_log")
+
 -- other modders
---require("sm0/TEST/export_helpers_why_grudge")
---require("sm0/TEST/export_helpers_whyll_defeated_traits")
---require("sm0/TEST/export_helpers_vauls_anvil")
---require("C:/Program Files (x86)/Steam/steamapps/common/Total War WARHAMMER II/data/script/campaign/mod/ultimate_chaos")
+require("sm0/z_BETA/!genevieve/script/export_helpers_why_grudge")
+require("sm0/z_BETA/!genevieve/script/export_helpers_whyll_defeated_traits")
 
-
---require("C:/Program Files (x86)/Steam/steamapps/common/Total War WARHAMMER II/data/script/campaign/main_warhammer/wh_dlc08_norscan_gods")
-
-require("sm0/battle_speed")
+require("sm0/z_BETA/supreme_anvil_of_vaul/script/export_helpers_vauls_anvil")
