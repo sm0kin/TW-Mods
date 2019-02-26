957d5ec4f20e387e08718f664e9d65be4d5b19d0
sm0kin <sm0kin.mod@gmail.com>
2019-02-26 17:02:47 +0100

    new

--- .vscode/entry.lua
+++ .vscode/entry.lua
@@ -26,7 +26,7 @@ require("sm0/sm0_mixedlores/script/ml_tables/ml_dlc07_brt_fay_enchantress")
 require("sm0/!!unlocker_uc/script/campaign/mod/unlocker_uc")
 --require("C:/Program Files (x86)/Steam/steamapps/common/Total War WARHAMMER II/data/script/campaign/mod/unlocker_uc")
 
-require("sm0/_ultimate_chaos_wh2/script/campaign/mod/ultimate_chaos")
+--require("sm0/_ultimate_chaos_wh2/script/campaign/mod/ultimate_chaos")
 --require("C:/Program Files (x86)/Steam/steamapps/common/Total War WARHAMMER II/data/script/campaign/mod/ultimate_chaos")
 --require("C:/Program Files (x86)/Steam/steamapps/common/Total War WARHAMMER II/data/script/campaign/main_warhammer/wh_dlc08_norscan_gods")
 --require("C:/Program Files (x86)/Steam/steamapps/common/Total War WARHAMMER II/data/script/campaign/main_warhammer/wh_chaos_invasion")
@@ -36,7 +36,7 @@ require("sm0/_ultimate_chaos_wh2/script/campaign/mod/ultimate_chaos")
 --require("sm0/ml_log")
 
 -- other modders
-require("sm0/z_BETA/!genevieve/script/export_helpers_why_grudge")
-require("sm0/z_BETA/!genevieve/script/export_helpers_whyll_defeated_traits")
+--require("sm0/z_BETA/!genevieve/script/export_helpers_why_grudge")
+--require("sm0/z_BETA/!genevieve/script/export_helpers_whyll_defeated_traits")
 
 require("sm0/z_BETA/supreme_anvil_of_vaul/script/export_helpers_vauls_anvil")
